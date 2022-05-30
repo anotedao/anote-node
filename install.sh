a1a96463859df9a715ebd5601b8a5c6b68bb8957
Pragmatic <cryptopragmatic@protonmail.com>
2022-05-30 23:02:42 +0200

    update

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ apt upgrade
 wget -c https://github.com/wavesplatform/Waves/releases/download/v1.3.15/waves_1.3.15_all.deb
 apt install -f ./waves_1.3.15_all.deb 
 mkdir /var/lib/aint
+wget https://raw.githubusercontent.com/aintdigital/aint-node/main/waves.conf
+wget https://raw.githubusercontent.com/aintdigital/aint-node/main/application.ini
+mv waves.conf /etc/waves/waves.conf
+mv application.ini /etc/waves/application.ini
