6615c3ef32edf474d57382e42bc0f53470d54d41
Pragmatic <cryptopragmatic@protonmail.com>
2022-06-10 22:10:48 +0200

    fix

--- waves.go
+++ waves.go
@@ -7,20 +7,6 @@ import (
 	"github.com/anonutopia/gowaves"
 )
 
-func balance() int {
-	a, err := gowaves.WNC.Addresses()
-	if err != nil {
-		log.Println(err.Error())
-	}
-	ar := *a
-
-	abr, err := gowaves.WNC.AddressesBalance(ar[0])
-	if err != nil {
-		log.Println(err.Error())
-	}
-	return abr.Balance
-}
-
 func initWaves() {
 	gowaves.WNC.Host = "http://localhost"
 	gowaves.WNC.Port = 6869
