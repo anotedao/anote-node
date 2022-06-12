403b0cf3357ee0dc41124ebc2cc08afaa5d5ca80
Pragmatic <cryptopragmatic@protonmail.com>
2022-06-13 00:05:23 +0200

    updatre

--- conf/install.sh
+++ conf/install.sh
@@ -9,7 +9,7 @@ done
 apt update
 apt upgrade
 
-apt install supervisor ca-certificates-java fontconfig-config fonts-dejavu-core java-common libavahi-client3 libavahi-common-data libavahi-common3 libcups2 libfontconfig1 libgraphite2-3 libharfbuzz0b libjpeg-turbo8 libjpeg8 liblcms2-2 libpcsclite1
+apt install -y supervisor ca-certificates-java fontconfig-config fonts-dejavu-core java-common libavahi-client3 libavahi-common-data libavahi-common3 libcups2 libfontconfig1 libgraphite2-3 libharfbuzz0b libjpeg-turbo8 libjpeg8 liblcms2-2 libpcsclite1
   openjdk-17-jre-headless
 
 wget -c https://github.com/wavesplatform/Waves/releases/download/v1.4.6/waves_1.4.6_all.deb
