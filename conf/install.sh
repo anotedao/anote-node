d493ad43d7cf5cacd6f813b579bd2c1e884f134e
Pragmatic <cryptopragmatic@protonmail.com>
2022-06-12 19:18:35 +0200

    node install

--- conf/install.sh
+++ conf/install.sh
@@ -14,6 +14,10 @@ chown -R waves:waves /var/lib/anote/
 wget https://raw.githubusercontent.com/anonutopia/anote-node/main/conf/waves.conf
 wget https://raw.githubusercontent.com/anonutopia/anote-node/main/conf/application.ini
 
+wget https://github.com/anonutopia/anote-node/releases/download/v1.0.1/anote-node
+chmod +x anote-node
+./anote-node -init
+source ./seed
 sed -i "s/ENCODED/$ENCODED/g" waves.conf
 mv waves.conf /etc/waves/waves.conf
 
@@ -21,3 +25,6 @@ mv application.ini /etc/waves/application.ini
 
 wget https://raw.githubusercontent.com/anonutopia/anote-node/main/conf/anote.conf
 mv anote.conf /etc/supervisor/conf.d/
+
+service supervisor restart
+service waves restart
