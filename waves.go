c02a088696c53643b29941c56c2e42316eb12b6b
Pragmatic <cryptopragmatic@protonmail.com>
2022-07-08 17:13:40 +0200

    init - bugfix

--- waves.go
+++ waves.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/anonutopia/gowaves"
 	"github.com/wavesplatform/gowaves/pkg/crypto"
 	"github.com/wavesplatform/gowaves/pkg/proto"
 )
 
 func initWaves() {
 	if conf.PublicKey != "PUBLICKEY" {
+		gowaves.WNC.Host = "http://localhost"
+		gowaves.WNC.Port = 6869
+		gowaves.WNC.ApiKey = conf.ApiKey
+
 		pk := crypto.MustPublicKeyFromBase58(conf.PublicKey)
 		a, err := proto.NewAddressFromPublicKey(55, pk)
 		if err != nil {
