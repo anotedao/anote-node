7967628fd660d694eb7bb9f74892a9d81587f4d3
Pragmatic <cryptopragmatic@protonmail.com>
2022-09-17 23:33:40 +0200

    node update

--- conf/update.sh
+++ conf/update.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+sudo supervisorctl stop anote
+rm anote-node
+apt remove supervisor
+
+wget -c https://github.com/wavesplatform/Waves/releases/download/v1.4.8/waves_1.4.8_all.deb
+wget https://github.com/anotedigital/anote-node/releases/download/v1.2.3/anote-node
+
+dpkg -i waves_1.4.8_all.deb
+chmod +x anote-node
+
+./anote-node -update
